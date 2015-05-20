1b96c20697e9e8a5fdd8654d0379acf68f5dbf4f
Sergey Bespalov <sergei.bespalov@gmail.com>
2015-05-20 10:46:07 +0300

    ch. 14 done

--- HelloMoon/app/src/main/java/bespalov/sergei/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/bespalov/sergei/hellomoon/HelloMoonFragment.java
@@ -18,6 +18,12 @@ public class HelloMoonFragment extends Fragment {
     private Button mStopButton;
     private Button mPauseButton;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
