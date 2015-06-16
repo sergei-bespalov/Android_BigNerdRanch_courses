d3386e0ffab8f68d9d862c2db01b47fc638d0afc
sergei-bespalov <sergei.bespalov@gmail.com>
2015-06-16 08:59:12 +0300

    ch.23 done withou Cexercise

---
+++ NerdLauncher/app/src/main/java/com/bespalov/sergey/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,80 @@
+package com.bespalov.sergey.nerdlauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by sergey on 16.06.15.
+ */
+public class NerdLauncherFragment extends ListFragment {
+    private static String TAG = "NerdListFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startUpIntent = new Intent(Intent.ACTION_MAIN);
+        startUpIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        final PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startUpIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities. ");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString()
+                );
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                activities){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                //because android.R.layout.simple_list_item_2 is TextView
+                TextView label = (TextView) view;
+                ResolveInfo info = getItem(position);
+                label.setText(info.loadLabel(pm));
+                return view;
+            }
+        };
+
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo) getListAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo == null) return;
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName(activityInfo.packageName, activityInfo.name);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(intent);
+    }
+}
---
+++ NerdLauncher/app/src/main/java/com/bespalov/sergey/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,37 @@
+package com.bespalov.sergey.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+public abstract class SingleFragmentActivity extends ActionBarActivity {
+    protected abstract Fragment createFragment();
+
+    public int getLayoutResourceId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResourceId());
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);*/
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
