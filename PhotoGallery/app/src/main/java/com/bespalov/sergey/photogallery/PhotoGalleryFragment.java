8e1449c12a668982f57e3f4b327c26d68b2b05e9
Sergey Bespalov <sergei.bespalov@gmail.com>
2015-06-30 10:57:24 +0300

    ch.26 PhotoGallery second commit

--- PhotoGallery/app/src/main/java/com/bespalov/sergey/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bespalov/sergey/photogallery/PhotoGalleryFragment.java
@@ -1,25 +1,31 @@
 package com.bespalov.sergey.photogallery;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.bespalov.sergey.photogallery.model.FlickrFetchr;
 import com.bespalov.sergey.photogallery.model.GalleryItem;
+import com.bespalov.sergey.photogallery.model.TumbnailDownloader;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
+    public static final String TAG = "PhotoGalleryFragment";
+
     private GridView mGridView;
     private ArrayList<GalleryItem> mItems;
-
-    public static final String TAG = "PhotoGalleryFragment";
+    TumbnailDownloader<ImageView> mTumbnailThread;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,18 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
 
         new FetchFlickrItemTask().execute();
+
+        mTumbnailThread = new TumbnailDownloader<>();
+        mTumbnailThread.start();
+        mTumbnailThread.getLooper();
+        Log.i(TAG, "Background thread started");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mTumbnailThread.quit();
+        Log.i(TAG, "Background thread destroyed");
     }
 
     @Nullable
@@ -61,9 +79,30 @@ public class PhotoGalleryFragment extends Fragment {
         if (getActivity() == null || mGridView == null) return;
 
         if (mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
+            mGridView.setAdapter(new GalleryAdapter(mItems));
         } else {
             mGridView.setAdapter(null);
+        }
+    }
+
+    private class GalleryAdapter extends ArrayAdapter<GalleryItem> {
+
+        public GalleryAdapter(List<GalleryItem> objects) {
+            super(getActivity(), 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item, parent, false);
+            }
+
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.drawable.brian_up_close);
+            GalleryItem item = getItem(position);
+            mTumbnailThread.queueThumbnail(imageView, item.getUrl());
+
+            return convertView;
         }
     }
 }
